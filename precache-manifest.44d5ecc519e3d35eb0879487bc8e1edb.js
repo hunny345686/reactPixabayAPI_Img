299f87da88bcfbd81c1907f7ee54bc23dae740cc
hunny345686 <hunny345686@gmail,com>
2020-09-12 17:38:07 +0530

    Updates

---
+++ precache-manifest.44d5ecc519e3d35eb0879487bc8e1edb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7927a6e8c62e70e60b579de48b5460d4",
+    "url": "/reactPixabayAPI_Img/index.html"
+  },
+  {
+    "revision": "62bb2aa5e7e87b3e7ee8",
+    "url": "/reactPixabayAPI_Img/static/css/main.212f7a9f.chunk.css"
+  },
+  {
+    "revision": "dd0e0acf0f9154dba0a0",
+    "url": "/reactPixabayAPI_Img/static/js/2.67659b33.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/reactPixabayAPI_Img/static/js/2.67659b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62bb2aa5e7e87b3e7ee8",
+    "url": "/reactPixabayAPI_Img/static/js/main.558d482c.chunk.js"
+  },
+  {
+    "revision": "c41b65699f73e1872d1d",
+    "url": "/reactPixabayAPI_Img/static/js/runtime-main.ba465c09.js"
+  }
+]);
